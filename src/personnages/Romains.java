574af497c13ab824d91981718116235f7c015cbc
Keunotorr <lois@baboum.org>
2026-03-31 08:36:22 +0200

    invariant pre post condition

--- src/personnages/Romains.java
+++ src/personnages/Romains.java
@@ -7,6 +7,7 @@ public class Romains {
 	public Romains(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert isInvariantVerified(force);
 		}
 	
 	public String getNom() {
@@ -22,13 +23,32 @@ public class Romains {
 		}
 	
 	public void recevoirCoup(int forceCoup) {
+		assert forceCoup > 0 : "la force du coup reçu est positive" ;
+		int f_temp = force;
 		force = force - forceCoup;
 		if (force < 1) {
 			parler("J'abandonne !");
 			prendreParole();
+			force = 0;
 		} else {
 			parler("Aïe");
 		}
+		assert isInvariantVerified(force);
+		assert f_temp > forceCoup : "la force d’un Romain a diminué";
 			
+	}
+	
+	private boolean isInvariantVerified(int force) {
+		if (force >= 0) {
+			return true;
+		}
+		else {
+			System.out.println("la force d’un Romain est toujours positive");
+			return false;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Romains minus = new Romains("Minus", 6);
 	}
 }
